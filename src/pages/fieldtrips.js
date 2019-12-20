d8c7e1e9be7594531639c01ed4d9078a372fec4f
jonfreed256 <github email address>
2019-12-19 20:22:47 -0500

    [ADDED] fieldtrips.js and workshops.js to /pages

--- src/pages/fieldtrips.js
+++ src/pages/fieldtrips.js
@@ -1,16 +1,15 @@
 import React from 'react';
-
 import Layout from '../components/layout';
 import Image from '../components/image';
 import SEO from '../components/seo';
 
-const BirthdayPage = () => (
+const FieltripsPage = () => (
   <Layout>
-    <SEO title='Birthday Parties' />
-    <h1>Birthday Parties</h1>
-    <p>Welcome to birthday parties page</p>
+    <SEO title='Fieldtrips' />
+    <h1>Fieldtrips</h1>
+    <p>Welcome to Fieldtrips page</p>
     <Image />
   </Layout>
 );
 
-export default BirthdayPage;
+export default FieltripsPage;
---
+++ src/pages/workshops.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
+
+const WorkshopPage = () => (
+  <Layout>
+    <SEO title='Workshops' />
+    <h1>Workshops</h1>
+    <p>Welcome to Workshops page</p>
+    <Image />
+  </Layout>
+);
+
+export default WorkshopPage;
